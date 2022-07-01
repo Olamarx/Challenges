68a2f948ac62d42c31639d82f01000ec5c1e2c4b
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-01 22:21:00 +0100

    First algorithm challenge

--- hashtagGenerator.js
+++ hashtagGenerator.js
@@ -8,11 +8,13 @@
 // If the final result is longer than 140 chars it must return false.
 // If the input or the result is an empty string it must return false.
 
-// Examples
 
+// Examples
 // " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
 // "    Hello     World   "                  =>  "#HelloWorld"
 // ""                                        =>  false
+
+
 function generateHashtag(str) {
   
     const split = str.split(' ')
