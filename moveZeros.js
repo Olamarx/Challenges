25e81e2c8647bf3fe2579cf51990ea83165d2c4e
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-02 15:37:26 +0100

    Second challenge of moving the zeros to the end

---
+++ moveZeros.js
@@ -0,0 +1,11 @@
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+function moveZeros(arr) {
+ const filterWords = arr.filter(zero => zero !== 0)
+ const filter = arr.filter(zero => zero === 0)
+ return [...filterWords, ...filter]
+  }
+
+  moveZeros([ 9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0 ])
