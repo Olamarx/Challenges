473adf804a50457af1644165d15b3996f80f00e0
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-03 08:28:45 +0100

    solved it afer many trials

--- challenges/pigIt.js
+++ challenges/pigIt.js
@@ -3,27 +3,40 @@
 // Examples
 // pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 // pigIt('Hello world !');     // elloHay orldway !
-
+//pigIt('O emporatay o oresmay !'); // Oay emporatay oay oresmay !
 function pigIt(str){
     //Code here
     let result = []
     const split = str.split(' ')
-    console.log(split)
+
     split.forEach(element => {
-        const first = element.charAt(0)
         const len = element.length
-        const others =  element.slice(1)
-
-        const checkothers = element.charAt(len - 1) === 'y' && element.charAt(len - 2) === 'a' ? element : others;
-        const checkFir = element.charAt(len - 1) === 'y' && element.charAt(len - 2) === 'a' ? '' : first;
-
-        const addOrNot = (element.charAt(len - 1) !== 'y' && element.charAt(len - 2) !== 'a') ? 'ay' : '';
         
-        result.push(`${checkothers}${checkFir}${addOrNot || others !== /a-z/ ? '' : 'ay' }`)
+        if ((/([a-zA-Z])/).test(element) && element.slice(len - 2) !== 'ay' ) {
+            result.push(`${element.substring(1)}${element[0]}ay`)
+        }
+        //  else if ((/([a-zA-Z])/).test(element)) {
 
-
+        // } 
+        
+        
+        else {
+            result.push(element)
+        }
+        
+        
     });
     console.log(result.join(' '))
   }
 
-  pigIt('O emporatay o oresmay !')
+  pigIt('hisTay siay my tringsay')
+
+        // const first = element.charAt(0)
+        // const others =  element.slice(1)
+        // const checkothers = element.charAt(len - 1) === 'y' && element.charAt(len - 2) === 'a' ? element : others;
+        // const checkFir = element.charAt(len - 1) === 'y' && element.charAt(len - 2) === 'a' ? '' : first;
+
+        // const addOrNot = (element.charAt(len - 1) !== 'y' && element.charAt(len - 2) !== 'a') ? 'ay' : '';
+
+        // result.push(`${checkothers}${checkFir}${ others === /a-z/ ? 'ay' : '' || addOrNot }`)
+//   substring() works like slice()
