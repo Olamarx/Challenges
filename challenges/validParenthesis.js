990ce403b64b0cda40e1dcde6bff5ebdae44b37e
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-04 11:31:11 +0100

    checked for valid parenthesis challenge

---
+++ challenges/validParenthesis.js
@@ -0,0 +1,32 @@
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+
+
+
+
+
+function validParentheses(parens) {
+    // your code here ..
+    // const obj = {
+    //     '(' : ')'
+    // }
+    const stack = []
+    for (let i = 0; i < parens.length; i++) {
+
+        const lastInStack = stack[stack.length - 1]
+
+        if (parens.charAt(i) ==='(') {
+            stack.push(parens.charAt(i))
+        } else if( lastInStack === '(' && parens.charAt(i) === ')' ) {
+            stack.pop()
+        } else return false
+    }
+   return stack.length ? false : true;
+  }
+
+  validParentheses("(())((()())())")
