4d46c97cc5ac6aa5d347830ee62eb49c3ed4bd81
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-04 11:36:20 +0100

    A clever approach

--- challenges/validParenthesis.js
+++ challenges/validParenthesis.js
@@ -30,3 +30,16 @@ function validParentheses(parens) {
   }
 
   validParentheses("(())((()())())")
+
+
+//   A CLEVER APPROACH
+//   function validParentheses(parens){
+//     var n = 0;
+//     for (var i = 0; i < parens.length; i++) {
+//       if (parens[i] == '(') n++;
+//       if (parens[i] == ')') n--;
+//       if (n < 0) return false;
+//     }
+    
+//     return n == 0;
+//   }
