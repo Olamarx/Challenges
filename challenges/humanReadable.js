fdd2ada4ebc6831697d59a081576b20384fc6122
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-05 14:03:54 +0100

    Trying today's challenge

--- challenges/humanReadable.js
+++ challenges/humanReadable.js
@@ -8,5 +8,31 @@
 // You can find some examples in the test fixtures.
 
 
+function humanReadable (seconds) {
+    if (seconds < 0 || seconds > 359999) {
+        return null
+    } else if (seconds === 0){
+        return "00:00:00"
+    } 
 
+    let hours = Math.floor(seconds / 3600)
+     seconds = seconds - (hours * 3600)
+
+     if (hours < 10) {
+        hours = '0' + hours
+     }
+
+     let minutes = Math.floor(hours / 60)
+     seconds = seconds - (minutes * 60)
+
+     if (minutes < 10) {
+        minutes = '0' + minutes
+     }
+
+     if (seconds < 10){
+        seconds = '0' + seconds
+     }
+
+     return `${hours}:${minutes}:${seconds}`
+  }
 
